a01c01393c54911b18eaef38562af7e306b07d48
Espen Klungland <espen.klungland@pwc.com>
2020-12-01 16:06:21 +0100

    day1

---
+++ Day1.py
@@ -0,0 +1,17 @@
+import numpy
+
+lFile = open('day1Input.txt','r')
+lines = lFile.read().splitlines()
+arr = numpy.array(lines)
+answer = 'The answer is {}. The integers are {} and {}'
+
+
+for line in arr:
+    amt = int(line)
+    remain = 2020-amt
+    count = numpy.count_nonzero(arr == str(remain))
+    if count >= 1:
+        x = numpy.where(arr==str(remain))
+        amt2 = int(arr[x[0]])
+        print(answer.format(str(amt*amt2),str(amt),str(amt2)))
+        break
