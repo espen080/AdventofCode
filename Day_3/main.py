9bb09f40004231cf18963e5bae263a447e4da554
Espen Klungland <espen.klungland@pwc.com>
2020-12-08 10:53:43 +0100

    day 3

--- Day_3/main.py
+++ Day_3/main.py
@@ -20,3 +20,22 @@ print(counter)
 
 'Part 2'
 
+def getTrees(r, d):
+    counter = 0
+    right = 0
+    down = 0
+
+    for line in lines:
+        right += r
+        down+=d
+        if down < len(lines):
+            nLine = lines[down]
+            while len(nLine) <= right:
+                nLine += nLine
+            if nLine[right]=='.':
+                pass
+            elif nLine[right]=='#':
+                counter+=1
+    return counter
+
+print(getTrees(1,1)*getTrees(3,1)*getTrees(5,1)*getTrees(7,1)*getTrees(1,2))
