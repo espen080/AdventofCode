c19357f7edb6aaf1d54909e6e91f6299df649b95
Espen Klungland <espen.klungland@pwc.com>
2020-12-09 09:05:42 +0100

    day 4

--- Day_4/main.py
+++ Day_4/main.py
@@ -17,13 +17,13 @@ for p in ps:
 print(counter)
 
 'Part 2'
-reqTags = {'byr':'(19[2-9][0-9])|(200[0-2])',
-        'iyr':'(201[0-9])|(2020)',
-        'eyr':'(202[0-9])|(2030)',
-        'hgt':'((1[5-8][0-9]cm)|(19[0-3]cm))|((59in)|(6[0-9]in)|(7[0-6]in))',
-        'hcl':'(#[0-9a-f]{6})',
+reqTags = {'byr':'^(19[2-9][0-9])$|^(200[0-2])$',
+        'iyr':'^(201[0-9])$|^(2020)$',
+        'eyr':'^(202[0-9])$|^(2030)$',
+        'hgt':'(^(1[5-8][0-9]cm)$|^(19[0-3]cm)$)|(^(59in)$|^(6[0-9]in)$|^(7[0-6]in)$)',
+        'hcl':'^(#[0-9a-f]{6})$',
         'ecl':'amb|blu|brn|gry|grn|hzl|oth',
-        'pid':'\d{9}'}
+        'pid':'^\d{9}$'}
 counter=0
 for p in ps:
     fields = p.replace('\n',' ').split(' ')
@@ -39,7 +39,6 @@ for p in ps:
             missing = True
         elif re.search(rx,value) == None:
             missing = True
-
     if not missing:
         counter+=1
 print(counter)
